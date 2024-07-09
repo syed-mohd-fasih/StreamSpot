9a6680760366e747e91f90e0a45919c9230e3605
Syed Fasih <elite9145build@gmail.com>
2024-07-09 10:16:26 +0300

    Created MoviesDirectoryServices

---
+++ StreamSpotMVC/Services/MoviesDirectoryService.cs
@@ -0,0 +1,48 @@
+﻿using StreamSpotMVC.Models;
+using System.Text.Json;
+
+namespace StreamSpotMVC.Services
+{
+    public class MoviesDirectoryService
+    {
+        // Save directory name where movies are saved
+        private string? MoviesDirectoryName => "D:\\Movies";
+
+        // Get all movies
+        // Assuming each sub directory has a .json file enclosing all data about the movie
+        public IEnumerable<Movie> GetMovies()
+        {
+            if (!Directory.Exists(MoviesDirectoryName))
+                return Enumerable.Empty<Movie>();
+
+            var directories = Directory.GetDirectories(MoviesDirectoryName);
+
+            var movies = new List<Movie>();
+
+            foreach (var d in directories)
+            {
+                var jsonFilePath = Path.Combine(d, "movie.json");
+
+                if (File.Exists(jsonFilePath))
+                {
+                    using (var jsonFileReader = File.OpenText(jsonFilePath))
+                    {
+                        var movie = JsonSerializer.Deserialize<Movie>(jsonFileReader.ReadToEnd(),
+                            new JsonSerializerOptions
+                            {
+                                PropertyNameCaseInsensitive = true,
+                                AllowTrailingCommas = true,
+                            }
+                        );
+                        if (movie != null)
+                        {
+                            movies.Add(movie);
+                        }
+                    }
+                }
+            }
+
+            return movies;
+        }
+    }
+}
