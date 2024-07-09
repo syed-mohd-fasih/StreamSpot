3bebb3e5906bcfeb90c9015844600ba4ca4a4d47
Syed Fasih <elite9145build@gmail.com>
2024-07-09 10:12:46 +0300

    Created Movie Model

---
+++ StreamSpotMVC/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StreamSpotMVC.Models
+{
+    public class Movie
+    {
+        public int Year { get; set; }
+        public string Title { get; set; }
+        public List<string> Tags { get; set; }
+        public string Description { get; set; }
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+        public double Rating { get; set; }
+    }
+}
