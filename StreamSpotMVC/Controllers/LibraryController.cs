bc6698a22fcd5c9b76c248c1e37f24eaa1b306b3
Syed Fasih <elite9145build@gmail.com>
2024-07-10 15:40:54 +0300

    Added data to db; to test and finalize library index page cards Added Movie detail/player view Added methods to Library controller to handle page data and video file fetch Added custom css to site.css

--- StreamSpotMVC/Controllers/LibraryController.cs
+++ StreamSpotMVC/Controllers/LibraryController.cs
@@ -8,8 +8,9 @@ namespace StreamSpotMVC.Controllers
     public class LibraryController : Controller
     {
         ApplicationDbContext _context;
+        private readonly string _videoDirectory = @"D:\Movies"; // Path to your video directory
 
-        public LibraryController(ApplicationDbContext context) 
+        public LibraryController(ApplicationDbContext context)
         {
             _context = context;
         }
@@ -19,6 +20,36 @@ namespace StreamSpotMVC.Controllers
             var movies = _context.Movies.ToList();
 
             return View(movies);
+        }
+
+        public IActionResult Movie(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+
+            var movie = _context.Movies.FirstOrDefault(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        [HttpGet("/library/video/{*fileName}")]
+        public IActionResult Play(string fileName)
+        {
+            var filePath = Path.Combine(_videoDirectory, fileName);
+
+            if (System.IO.File.Exists(filePath))
+            {
+                var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                return File(fileStream, "video/mp4"); // Adjust MIME type based on your video format
+            }
+
+            return NotFound();
         }
     }
 }
